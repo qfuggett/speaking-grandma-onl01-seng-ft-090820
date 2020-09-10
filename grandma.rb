3d800d2f9f815cb22fe122d4e9db803320f5dafb
QueenTesa Fuggett <qfuggett@gmail.com>
2020-09-10 01:58:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,10 +15,5 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  else if phrase != phrase.upcase
-    return "HUH?! SPEAK UP, SONNY!"
-  else phrase == phrase.upcase 
-    return "NO, NOT SINCE 1938"
-  end
 end
 
